02d9d362d72cce4621187cb67c0286b9c9a88aee
George-Medeiros <george.emerson.med@gmail.com>
2026-02-10 11:03:30 +0000

    Add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def main():
+    valores = np.array([1, 2, 3, 4, 5])
+    print("Valores:", valores)
+    print("Soma:", np.sum(valores))
+
+if __name__ == "__main__":
+    main()
